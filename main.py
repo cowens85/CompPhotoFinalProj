43346bde53393223cc8631d8cb8ab2fe16f7b1ec
Jake Ritter <RitterGT@gmail.com>
2015-07-08 21:57:25 -0400

    added stuff

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-__author__ = 'chris'
+import numpy as np
+import scipy as sp
+import scipy.signal
+import cv2
+
+# Import ORB as SIFT to avoid confusion.
+try:
+    from cv2 import ORB as SIFT
+except ImportError:
+    try:
+        from cv2 import SIFT
+    except ImportError:
+        try:
+            SIFT = cv2.ORB_create
+        except:
+            raise AttributeError("Your OpenCV(%s) doesn't have SIFT / ORB."
+                                 % cv2.__version__)
+
+def drawAroundBanana():
+    imagePath = "images/source/card1.jpg"
+    cascPath = "cascade/haar.xml"
+
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    # Read the image
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.array([0.25 - 0.4 / 2.0, 0.25, 0.4, 0.25, 0.25 - 0.4 / 2.0])
+
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        image,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(100, 100),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    print "Found {0} faces!".format(len(faces))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+    cv2.imshow("Faces found", image)
+    cv2.waitKey(0)
+
+drawAroundBanana()
+exit(1)
