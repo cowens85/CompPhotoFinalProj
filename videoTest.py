449185c8dffa677a04e4db9abffd2121a46bcc32
Jake Ritter <RitterGT@gmail.com>
2015-07-13 20:44:27 -0400

    added a quick brute force optimizer and ran on video

--- videoTest.py
+++ videoTest.py
@@ -43,10 +43,11 @@ def drawAroundBanana():
 
         cascade = cv2.CascadeClassifier(cascPath)
         # Detect object in the image
+
         faces = cascade.detectMultiScale(
             gray,
-            scaleFactor=1.8,
-            minNeighbors=2,
+            scaleFactor=2.6,
+            minNeighbors=1,
             minSize=(200, 200),
             flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
@@ -54,17 +55,19 @@ def drawAroundBanana():
         print "Found {0} objects!".format(len(faces))
 
         newFrame = background.copy()
-        # Draw a rectangle around the faces
+       # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(gray, (x, y-150), (x+w, y+h+50), (0, 255, 0), 2)
             newFrame[y-150:y+h+50, x:x+w] = frame[y-150:y+h+50, x:x+w]
+            break
+
 
         # Display the resulting frame
         cv2.imshow('frame',newFrame)
         if(len(faces) > 0):
             vout.write(newFrame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+           break
 
     # When everything done, release the capture
     print background.shape
@@ -73,5 +76,45 @@ def drawAroundBanana():
     cv2.destroyAllWindows()
 
 
+
+def countBananaFrames():
+    highestValue = (0,0,0)
+    scale = 1.1
+    minNeighbor = 0
+    while(scale < 3.0):
+        for minNeighbor in range(0, 5):
+            cap = cv2.VideoCapture("video/banan.m4v")
+            counter = 0
+            while(True):
+                # Capture frame-by-frame
+                ret, frame = cap.read()
+                if frame is None:
+                    break
+
+                # Our operations on the frame come here
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cascPath = "cascade/banana_classifier.xml"
+
+                cascade = cv2.CascadeClassifier(cascPath)
+                # Detect object in the image
+
+                faces = cascade.detectMultiScale(
+                    gray,
+                    scaleFactor=scale,
+                    minNeighbors=minNeighbor,
+                    minSize=(200, 200),
+                    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+                )
+                if len(faces) == 1:
+                    counter+=1
+
+            if(highestValue[0] < counter):
+                highestValue = (counter, scale, minNeighbor)
+            print "Did: " + str(scale) + " " + str(minNeighbor) + " Result: " + str(counter)
+
+            cap.release()
+            cv2.destroyAllWindows()
+        scale += .1
+        print highestValue
 drawAroundBanana()
 exit(1)
